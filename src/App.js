7c3cd52973e1bf8657de70f27d47f0598dc5ab4b
Marc Majcher <majcher@gmail.com>
2021-10-13 19:19:21 -0500

    testfetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import TaskList from './TaskList';
+
+const TaskURL = "http://localhost:3000/tasks"
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch(TaskURL).then(res => res.json()).then(json => setTasks(json));
+  }, [])
+
   return (
     <div className="App">
       <h1>One Thing Well</h1>
+      <h2>Testing Tasks:</h2>
+      <TaskList tasks={tasks} />
     </div>
   );
 }
